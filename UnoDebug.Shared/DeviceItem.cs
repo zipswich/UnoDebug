2a4b1f378f3fa34f6c15569dfd54b514a5686cc7
Hong Zhang <h9124@msn.com>
2019-06-11 11:14:24 -0400

    Add project files.

---
+++ UnoDebug.Shared/DeviceItem.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UnoDebug
+{
+    public class DeviceItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string sName { get; set; }
+        string _sSatus;
+        public string sStatus
+        {
+            get { return _sSatus; }
+            set
+            {
+                _sSatus = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string sType { get; set; }
+        public string sCapabilities { get; set; }
+
+        BitmapImage _bmp;
+        public BitmapImage bmp
+        {
+            get { return _bmp; }
+            set
+            {
+                _bmp = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public double iDebug
+        {
+            get { return 300; }
+        }
+
+
+        double _iTileWidth = 300;
+        public double iTileWidth
+        {
+            get { return _iTileWidth; }
+            set
+            {
+                _iTileWidth = value;
+                iTileHeight = value * 3 / 4;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        double _iTileHeight = 200;
+        public double iTileHeight
+        {
+            get { return _iTileHeight; }
+            set
+            {
+                _iTileHeight = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        Thickness _iImageMargin = new Thickness(0);
+        public Thickness iImageMargin
+        {
+            get { return _iImageMargin; }
+            set
+            {
+                _iImageMargin = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+        public double iTileFontSize
+        {
+            get
+            {
+                double iFontSize = Math.Max(7, iTileWidth / 15);
+                iFontSize = Math.Min(20, iFontSize);
+                return iFontSize;
+            }
+        }
+
+
+    }
+}
---
+++ UnoDebug.Shared/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoDebug
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public ObservableCollection<DeviceItem> _listDeviceItems = new ObservableCollection<DeviceItem>();
+        public ObservableCollection<DeviceItem> listDeviceItems
+        {
+            get { return _listDeviceItems; }
+            set
+            {
+                _listDeviceItems = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        double _iTileWidth = 300;
+        public double iTileWidth
+        {
+            get { return _iTileWidth; }
+            set
+            {
+                _iTileWidth = value;
+                iTileHeight = value * 3 / 4;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        double _iTileHeight = 200;
+        public double iTileHeight
+        {
+            get { return _iTileHeight; }
+            set
+            {
+                _iTileHeight = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        DeviceItem _diSelected = null;
+        public DeviceItem diSelected
+        {
+            get { return _diSelected; }
+            set
+            {
+                if (_diSelected == value)
+                {
+                    //do nothing
+                }
+                else
+                {
+                    _diSelected = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _listDeviceItems.Add(new DeviceItem
+            {
+                sName = "Test1",
+                iTileWidth = 300,
+            });
+            _listDeviceItems.Add(new DeviceItem
+            {
+                sName = "Test2",
+                iTileWidth = 300,
+            });
+            _listDeviceItems.Add(new DeviceItem
+            {
+                sName = "Test3",
+                iTileWidth = 300,
+            });
+        }
+
+        private void BtTest_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Debug.Write("Button is clicked");
+        }
+
+        private void Grid_Holding(object sender, Windows.UI.Xaml.Input.HoldingRoutedEventArgs e)
+        {
+            Debug.Write("Holding has worked.");
+        }
+
+        private void LvDeviceTiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Debug.Write("Selected item:" + diSelected.ToString());
+        }
+    }
+}
